ef332fd04d7848576cd917b03eebdb009712e5fd
Calvin McElroy <fourwallsstudio@gmail.com>
2017-08-15 15:28:20 -0400

    remove config

--- server/db.js
+++ server/db.js
@@ -7,8 +7,6 @@ if (process.env.DATABASE_URL) {
   db = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
-    port:     match[4],
-    host:     match[3],
     logging: false
   })
 } else {
