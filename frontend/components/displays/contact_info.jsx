0dd6208e513a39a1895931c1aaf60edafb92637b
Calvin McElroy <fourwallsstudio@gmail.com>
2017-12-06 14:42:40 -0500

    change email

--- frontend/components/displays/contact_info.jsx
+++ frontend/components/displays/contact_info.jsx
@@ -4,7 +4,7 @@ const ContactInfo = () => {
   return (
     <div className="contact-container">
       <a href="http://calvinmcelroy.us/" target="_blank"><p>calvinmcelroy.us</p></a>
-      <a href="mailto:fourwallsstudio@gmail.com"><p>fourwallsstudio@gmail.com</p></a>
+      <a href="mailto:calvin.mcelroy.dev@gmail.com"><p>calvin.mcelroy.dev@gmail.com</p></a>
       <div className="contact-icons-box">
         <a href="https://github.com/fourwallsstudio" target="_blank" className="github-img">
           <img src="assets/github.png"></img>
@@ -12,7 +12,7 @@ const ContactInfo = () => {
         <a href="https://angel.co/calvin-mcelroy-1" target="_blank" className="angellist-img">
           <img src="assets/angellist.png"></img>
         </a>
-        <a href="https://www.linkedin.com/in/calvin-mcelroy-04253210b/" target="_blank" className="linkedin-img">
+        <a href="https://www.linkedin.com/in/calvin-mcelroy-dev/" target="_blank" className="linkedin-img">
           <img src="assets/linkedin.png"></img>
         </a>
       </div>
