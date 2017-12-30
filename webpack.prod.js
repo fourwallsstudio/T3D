941e68a4508fbe39c101f8a70ad6ce9ee2aa5661
Calvin McElroy <fourwallsstudio@gmail.com>
2017-12-30 16:51:57 -0500

    --amend

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,4 @@
-const merger = require('webpack-merge');
+const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
